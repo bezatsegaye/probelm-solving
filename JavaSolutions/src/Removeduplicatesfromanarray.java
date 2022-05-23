78ce86722591c279e4f8b08f063bcbeed831f7ca
bezatsegaye <tsegaye@gmail.com>
2022-05-22 19:36:58 -0500

    added new palindrome and remove duplicate

---
+++ JavaSolutions/src/Removeduplicatesfromanarray.java
@@ -0,0 +1,20 @@
+import com.sun.source.doctree.SeeTree;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Removeduplicatesfromanarray {
+
+    public static Set<Integer> RemoveDuplicate (int [] arr){
+        Set<Integer> st = new HashSet<>();
+        for(int i=0; i< arr.length; i++) {
+            st.add(arr[i]);
+        }
+        return st;
+    }
+    public static void main(String[] args) {
+    System.out.println(Removeduplicatesfromanarray.RemoveDuplicate(
+            new int [] {1,1,2,3,3,4,5}));
+    }
+}
+
